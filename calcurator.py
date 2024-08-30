b71419b81d420bfaa318ad0f13333a26ec231c4f
purulek <adam.purul@gmail.com>
2024-08-30 23:26:16 +0200

    third commit

--- calcurator.py
+++ calcurator.py
@@ -17,4 +17,34 @@ button2.grid(row=0, column=1, padx=10, pady=10)
 
 button3 = tk.Button(root, text=3, command=lambda:on_button_click(3))
 button3.grid(row=0, column=2, padx=10, pady=10)
+button4 = tk.Button(root, text=4, command=lambda:on_button_click(4))
+button4.grid(row=1, column=0, padx=10, pady=10)
+
+
+button5 = tk.Button(root, text=5, command=lambda:on_button_click(5))
+button5.grid(row=1, column=1, padx=10, pady=10)
+
+
+
+
+button6 = tk.Button(root, text=6, command=lambda:on_button_click(3))
+button6.grid(row=1, column=2, padx=10, pady=10)
+
+
+
+
+
+button7 = tk.Button(root, text=7, command=lambda:on_button_click(3))
+button7.grid(row=2, column=0, padx=10, pady=10)
+
+
+
+
+button8 = tk.Button(root, text=8, command=lambda:on_button_click(8))
+button8.grid(row=2, column=1, padx=10, pady=10)
+
+
+
+button9 = tk.Button(root, text=9, command=lambda:on_button_click(9))
+button9.grid(row=2, column=2, padx=10, pady=10)
 root.mainloop()

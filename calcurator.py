1b3456d62667c9ec5acafaf01e7f69d6b132c1f6
purulek <adam.purul@gmail.com>
2024-09-06 22:45:15 +0200

    next commit

--- calcurator.py
+++ calcurator.py
@@ -13,10 +13,13 @@ def on_button_click_number(button_name):
 
 
 def on_button_click_operation(button_name):
+    global number
     number = (''.join (numbers))
-    
+    numbers.pop()
+    numbers.append(number)
+    print(numbers)
     operation.append(button_name)
-
+    
 
 def on_button_click_exe():
     messagebox.showinfo("hi", number)
@@ -47,10 +50,10 @@ button_divison.grid(row=0, column=3, padx=10, pady=10)
 button1 = tk.Button(root, text=1, command=lambda:on_button_click_number("1"))
 button1.grid(row=1, column=0, padx=10, pady=10)
 
-button2 = tk.Button(root, text=2, command=lambda:on_button_click_number(2))
+button2 = tk.Button(root, text=2, command=lambda:on_button_click_number("2"))
 button2.grid(row=1, column=1, padx=10, pady=10)
 
-button3 = tk.Button(root, text=3, command=lambda:on_button_click_number(3))
+button3 = tk.Button(root, text=3, command=lambda:on_button_click_number("3"))
 button3.grid(row=1, column=2, padx=10, pady=10) 
 
 button_multi = tk.Button(root, text="", command=lambda:on_button_click_operation(""))
@@ -60,13 +63,13 @@ button_multi.grid(row=1, column=3, padx=10, pady=10)
 
 
 
-button4 = tk.Button(root, text=4, command=lambda:on_button_click_number(4))
+button4 = tk.Button(root, text=4, command=lambda:on_button_click_number("4"))
 button4.grid(row=2, column=0, padx=10, pady=10)
 
-button5 = tk.Button(root, text=5, command=lambda:on_button_click_number(5))
+button5 = tk.Button(root, text=5, command=lambda:on_button_click_number("5"))
 button5.grid(row=2, column=1, padx=10, pady=10)
 
-button6 = tk.Button(root, text=6, command=lambda:on_button_click_number(6))
+button6 = tk.Button(root, text=6, command=lambda:on_button_click_number("6"))
 button6.grid(row=2, column=2, padx=10, pady=10)
 
 button_minus= tk.Button(root, text="-", command=lambda:on_button_click_operation("-"))
@@ -75,13 +78,13 @@ button_minus.grid(row=2, column=3, padx=10, pady=10)
 
 
 
-button7 = tk.Button(root, text=7, command=lambda:on_button_click_number(7))
+button7 = tk.Button(root, text=7, command=lambda:on_button_click_number("7"))
 button7.grid(row=3, column=0, padx=10, pady=10)
 
-button8 = tk.Button(root, text=8, command=lambda:on_button_click_number(8))
+button8 = tk.Button(root, text=8, command=lambda:on_button_click_number("8"))
 button8.grid(row=3, column=1, padx=10, pady=10)
 
-button9 = tk.Button(root, text=9, command=lambda:on_button_click_number(9))
+button9 = tk.Button(root, text=9, command=lambda:on_button_click_number("9"))
 button9.grid(row=3, column=2, padx=10, pady=10)
 
 button_plus = tk.Button(root, text="+", command=lambda:on_button_click_operation("+"))
@@ -90,7 +93,7 @@ button_plus.grid(row=3, column=3, padx=10, pady=10)
 
 
 
-button0= tk.Button(root, text=0, command=lambda:on_button_click_number(0))
+button0= tk.Button(root, text=0, command=lambda:on_button_click_number("0"))
 button0.grid(row=4, column=0, padx=10, pady=10) 
 
 button_coma= tk.Button(root, text=",", command=lambda:on_button_click_operation(","))
@@ -118,5 +121,3 @@ button_factorial= tk.Button(root, text="!", command=lambda:on_button_click_opera
 button_factorial.grid(row=5, column=3, padx=10, pady=10)
 
 root.mainloop()
-def reset (): 
-    root.mainloop()
